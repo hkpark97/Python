7b9c227ba272073f89c2f3bea37940b8772b8c14
kate <tbtehf12@naver.com>
2022-02-08 14:21:59 -0500

    chap04

--- Chap04/ternary.py
+++ Chap04/ternary.py
@@ -1,6 +1,11 @@
 #!/usr/bin/env python3
 # Copyright 2009-2017 BHG http://bw.org/
 
-hungry = True
+# 0 works fine for False
+hungry = False
 x = 'Feed the bear now!' if hungry else 'Do not feed the bear.'
 print(x)
+
+
+# The ternary operator is a type of conditional expression in Python that evaluates a statement. 
+# Ternary operators perform an action based on whether that statement is true or false.
